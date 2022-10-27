1403afd7e4a61a78f0d7a5b24abc743d91cce776
JoanRamon <joanramonraluy@gmail.com>
2022-10-27 11:47:56 +0200

    Patching servide.js to run correctly

--- DApps/DAO-Dapp/js/dao.js
+++ DApps/DAO-Dapp/js/dao.js
@@ -61,7 +61,7 @@ var VAULT_KEY1 = "";
 var VAULT_KEY2 = "";
 var VAULT_KEY3 = "";
 
-MDS.init(function(msg){
+/*MDS.init(function(msg){
   //Do initialitzation
   if(msg.event == "inited"){
     MDS.log("The DAO app init...");
@@ -83,7 +83,7 @@ MDS.init(function(msg){
     // user's balance has changed
     MDS.log("New Balance Detected");
 		//Process the new event detectedload
-    newBalanceEvent();
+    //newBalanceEvent();
     getcontacts();
     getTokens();
     loadAdvertisingTokensFromNode();
@@ -186,8 +186,33 @@ function createTheDBAdvertisers(msg){
 			MDS.log("DB DAO Profiles Inited..");
 		});
 }
+*/
 
+//This function just create the sendpoll if it is not yet
+function preparingSendpoll(){
+  MDS.cmd("sendpoll action:list", function(res){
+    if (res.status) {
+      MDS.log("Number of sendpoll: "+res.response.total);
+      if (res.response.total == 0){
+        MDS.log("Creating a uid sendpoll");
+				MDS.cmd("sendpoll action:add", function(res){
+			    if (res.status) {
 
+          }else{
+			      var nodeStatus = JSON.stringify(res, undefined, 2);
+			      document.getElementById("status-object").innerText = nodeStatus;
+			      MDS.log(JSON.stringify(res));
+			    }
+			  });
+      }
+    }
+    else{
+      var nodeStatus = JSON.stringify(res, undefined, 2);
+      document.getElementById("status-object").innerText = nodeStatus;
+      MDS.log(JSON.stringify(res));
+    }
+  });
+}
 
 
 /////*****MAXIMA SECTION
